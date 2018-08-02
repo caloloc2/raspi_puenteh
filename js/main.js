071de18da2eaa2785d2518d9006e67e8898e3037
caloloc2 <calolomino@gmail.com>
2018-08-02 09:26:25 -0500

    actualiza

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
-
+$(document).keypress(function(event) {
+	/* Act on the event */
+	tecla = event.which;
+	switch (tecla) {
+		case 113:
+			console.log('presionando...', 1);
+			Guarda_Valor(1);
+			break;
+		case 119:
+			// tecla w
+			console.log('presionando...', 2);
+			Guarda_Valor(2);
+			break;
+		case 101:
+			// tecla e
+			console.log('presionando...', 3);
+			Guarda_Valor(3);
+			break;
+		case 97:
+			// tecla a
+			console.log('presionando...', 4);
+			Guarda_Valor(4);
+			break;
+		case 115:
+			// tecla s
+			console.log('presionando...', 0);
+			Guarda_Valor(0);
+			break;
+		case 100:
+			// tecla d
+			console.log('presionando...', 5);
+			Guarda_Valor(5);
+			break;
+		case 122:
+			// tecla z
+			console.log('presionando...', 6);
+			Guarda_Valor(6);
+			break;
+		case 120:
+			// tecla x
+			console.log('presionando...', 7);
+			Guarda_Valor(7);
+			break;
+		case 99:
+			// tecla c
+			console.log('presionando...', 8);
+			Guarda_Valor(8);
+			break;		
+	}
+});
 
 $('.linea .item').mouseup(function(event) {
 	/* Act on the event */
